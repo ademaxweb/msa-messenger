fd6b7109c2a482e78b46e3ffaa58ae3244259f3f
ademaxweb <lopas1902@gmail.com>
2025-10-09 01:33:52 +0300

    added generateChatName function to Chat service

--- chat/internal/app/usecases/create_direct_chat.go
+++ chat/internal/app/usecases/create_direct_chat.go
@@ -3,12 +3,20 @@ package usecases
 import (
 	"chat/internal/app/models"
 	"chat/internal/app/usecases/dto"
+	"fmt"
+	"math/rand"
 )
+
+func generateChatName() string {
+	colors := []string{"Красный", "Зелёный", "Жёлтый", "Розовый", "Голубой", "Оранжевый"}
+
+	return fmt.Sprintf("%s чат №%d", colors[rand.Intn(len(colors))], rand.Intn(10000)+1)
+}
 
 func (cs *ChatService) CreateDirectChat(o dto.CreateDirectChat) (*models.Chat, error) {
 	c := &models.Chat{
-		Name:        "New chat", // TODO add chat naming logic
-		Description: "",         // TODO add chat description logic
+		Name:        generateChatName(),
+		Description: "", // TODO add chat description logic
 		AvatarURL:   "",
 	}
 
